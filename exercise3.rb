9bb4bdd393b08ea2f28f5cecaff5f881e9ec6ae9
Sam Pollock <samcpollock@gmail.com>
2014-06-03 13:46:29 -0400

    complete exercise4

--- exercise3.rb
+++ exercise3.rb
@@ -3,4 +3,4 @@ name = gets.chomp
 puts "Hi #{name}!"
 puts "And how old are you, #{name}?"
 year_of_birth = Time.now.year - gets.chomp.to_i
-puts "Here lies #{name}, #{year_of_birth} - 2014"
+puts "Here lies #{name}, #{year_of_birth} - 2014 "
